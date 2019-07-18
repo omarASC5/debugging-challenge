369badeb18abeee5cd9a5e49b2bb49511ff01c3f
Patrick_B <pb5191@gmail.com>
2019-07-18 19:55:17 -0400

    hard one added

--- hard/point-of-view.js
+++ hard/point-of-view.js
@@ -1,5 +1,13 @@
-function promptDirection(question) {
-  let result = prompt(question);
+function prompt(str) {
+  const lastIndex = str.length - 1;
+  if (str.charCodeAt(lastIndex) === 63) {
+    return "l3fT";
+  }
+  return "riGHt";
+}
+
+function promptDirection(str) {
+  let result = prompt(str);
   if (result.toLowerCase() === "left") return "L";
   if (result.toLowerCase() === "right") return "R";
   throw new Error("Invalid direction: " + result);
@@ -18,3 +26,6 @@ try {
 } catch (error) {
   console.log("Something went wrong: " + error);
 }
+// Expected result: "You see two angry bears"
+
+// How to debug and modify the code to achieve the expected result?
